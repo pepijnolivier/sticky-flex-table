b6acf7d600391aee029f7863fe3ce1af7ab479de
Pepijn Olivier <olivier.pepijn@gmail.com>
2020-02-17 00:43:03 +0100

    add build

---
+++ examples/1.demo/build/precache-manifest.8d950c07be8f17929253a9603b764a0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "607318e4bb6aaa120db09524055a20cc",
+    "url": "/sticky-flex-table/examples/1.demo/build/index.html"
+  },
+  {
+    "revision": "64c46a0be43d759738b3",
+    "url": "/sticky-flex-table/examples/1.demo/build/static/css/main.aaba24d5.chunk.css"
+  },
+  {
+    "revision": "682d76d0fce18cfbc178",
+    "url": "/sticky-flex-table/examples/1.demo/build/static/js/2.9b2bc070.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/sticky-flex-table/examples/1.demo/build/static/js/2.9b2bc070.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64c46a0be43d759738b3",
+    "url": "/sticky-flex-table/examples/1.demo/build/static/js/main.725a0af9.chunk.js"
+  },
+  {
+    "revision": "e8417a5abbb355f74acc",
+    "url": "/sticky-flex-table/examples/1.demo/build/static/js/runtime-main.3fbb2672.js"
+  }
+]);
---
+++ examples/1.demo/build/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/sticky-flex-table/examples/1.demo/build/precache-manifest.8d950c07be8f17929253a9603b764a0d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sticky-flex-table/examples/1.demo/build/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
